a5042ba8c242c7ad0363ce4efc749f62df042b59
Echilon <echilon30@gmail.com>
2026-02-13 18:47:13 -0500

    Delete .config/hypr/Scripts directory

--- .config/hypr/Scripts/show-keyhints.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Call the Python script and feed its output into rofi
-python3 ~/.config/hypr/Scripts/keyhints.py --format rofi \
-  | rofi -p "Keybinds" -dmenu \
-    -theme-str 'window { width: 40%; } listview { spacing: 15px; }'
